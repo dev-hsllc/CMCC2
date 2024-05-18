d4919dc29fb4db01105fac22218b74a5e75a22cd
dev-hsllc <developer.hsllc@gmail.com>
2024-05-18 11:45:26 +0000

    updates-5-18

--- calculator.py
+++ calculator.py
@@ -35,11 +35,14 @@ def format_dollar_value(value):
     return f"${value:,}"
 
 # Title of the app
-st.title('Contract Manager Pricing Estimator!')
+st.title('Contract Manager Pricing Estimator v01')
 
 # Blurb at the top of the page
 st.markdown("""
 Interested in Contract Manager for your organization? Just move the sliders below to best reflect the revenue and contract demands of your organization, and we'll give you some ballpark pricing. **NOTE:** All estimates are purely for informational purposes; actual customized pricing will likely vary once we understand the nuances of your specific situation.
+""")
+st.markdown("""
+**NOTE:** All estimates are purely for informational purposes; actual customized pricing will likely vary once we understand the nuances of your specific situation.
 """)
 
 # User inputs using sliders
@@ -53,25 +56,103 @@ average_pages_per_contract = st.slider('Select average pages per contract:', min
 
 # Button to calculate pricing
 if st.button('Calculate Pricing'):
-    # Calculation of Base Fee using log regression
+    price_max = 2000000000
     fee_percent = -0.0000194472 * math.log(annual_revenue) + 0.0004408921
-    base_fee = annual_revenue * fee_percent
-    
-    # PLL Costs and Variable Costs
-    pll_costs = base_fee * 0.20
-    variable_costs = base_fee * 0.39 * (annual_contract_volume * average_pages_per_contract * 0.001)
+    discount = .10
+    setup_costs = 8500
 
-    # Foundation Fee calculation
-    foundation_fee = base_fee + pll_costs + variable_costs
+    # Base Fee
+    if annual_revenue > price_max:
+	    base_fee = annual_revenue * price_max
+    else:
+        base_fee = max(9500, annual_revenue * fee_percent)
+    base_fee = math.trunc(base_fee)
 
-    # Calculating further tiers
-    framework_fee = foundation_fee * 1.4
-    pinnacle_fee = framework_fee * 1.4
+    # Variable Costs
+
+    # Litera License
+    if base_fee < 21000:
+	    litera_license = 500
+    elif base_fee in range(21001, 50000):
+	    litera_license = 2000
+    else:
+	    litera_license = 4000
+    litera_license = math.trunc(litera_license)
+
+    #CIDA MS Azure
+    cida_ms_azure = (annual_contract_volume * average_pages_per_contract * .15)
+
+    #first_rule
+    first_rule = (base_fee * .20)
+
+    #Variable Costs Total
+    variable_costs = round(math.trunc(litera_license + cida_ms_azure + first_rule), -1) 
+
+    #Total Discount
+    total_discount = math.trunc(base_fee * .10 + first_rule * discount)
+
+
+    # Caculate Teir Base 
+    foundation_base = round(math.trunc(base_fee + variable_costs - total_discount))
+    framework_base = round(math.trunc(foundation_base * 1.49))
+    pinnacle_base = round(math.trunc(foundation_base * 1.95) -4) #FIGURE A BETTER WAY TO ROUND THIS
+
+
+    # PLL Costs
+    if base_fee < 7500:
+        pll_costs = 1500 #CHANGE TO 1650
+    elif base_fee in range(7501, 20000):
+        pll_costs = base_fee * .20
+    elif base_fee in range(20001, 45000):
+        pll_costs = base_fee * .175
+    elif base_fee in range(45001, 150000):
+        pll_costs = base_fee * .15
+    else:
+        pll_costs = base_fee * .125
+        
+    pll_costs = math.trunc(pll_costs - pll_costs * discount)
+
+
+    # PLL Costs Framework
+    if framework_base < 7500:
+        pll_costs_framework = 1500 #CHANGE TO 1650
+    elif framework_base in range(7501, 20000):
+        pll_costs_framework = framework_base * .20
+    elif framework_base in range(20001, 45000):
+        pll_costs_framework = framework_base * .175
+    elif framework_base in range(45001, 150000):
+        pll_costs_framework = framework_base * .15
+    else:
+        pll_costs_framework = framework_base * .125
+        
+    pll_costs_framework = math.trunc(pll_costs_framework)
+
+
+    # PLL Costs Pinnacle
+    if pinnacle_base < 7500:
+        pll_costs_pinnacle = 1500 #CHANGE TO 1650
+    elif pinnacle_base in range(7501, 20000):
+        pll_costs_pinnacle = pinnacle_base * .20
+    elif pinnacle_base in range(20001, 45000):
+        pll_costs_pinnacle = pinnacle_base * .175
+    elif pinnacle_base in range(45001, 150000):
+        pll_costs_pinnacle = pinnacle_base * .15
+    else:
+        pll_costs_pinnacle = pinnacle_base * .125
+        
+    pll_costs_pinnacle = math.trunc(pll_costs_pinnacle)
+
+    # Teir Total calculation
+    foundation_total = round(foundation_base + pll_costs + setup_costs, -1)
+    framework_total = round(framework_base + pll_costs_framework + setup_costs, -1)
+    pinnacle_total = round(pinnacle_base + pll_costs_pinnacle + setup_costs, -1)
 
     # Display the formatted results
     st.markdown(f'Thank you for your interest in Contract Manager. Based on the inputs you\'ve provided, a ballpark estimate for your organization is:', unsafe_allow_html=True)
-    st.markdown(f'<div class="pricing-output">Foundation Tier annual pricing is: <span class="dollar">${foundation_fee:,.2f}</span></div>', unsafe_allow_html=True)
-    st.markdown(f'<div class="pricing-output">Framework Tier annual pricing is: <span class="dollar">${framework_fee:,.2f}</span></div>', unsafe_allow_html=True)
-    st.markdown(f'<div class="pricing-output">Pinnacle Tier annual pricing is: <span class="dollar">${pinnacle_fee:,.2f}</span></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="pricing-output">Foundation Tier annual pricing is: <span class="dollar">${foundation_total:,.2f}</span></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="pricing-output">Framework Tier annual pricing is: <span class="dollar">${framework_total:,.2f}</span></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="pricing-output">Pinnacle Tier annual pricing is: <span class="dollar">${pinnacle_total:,.2f}</span></div>', unsafe_allow_html=True)
+
+
 
 # Run this with `streamlit run this_script.py`
