f7d48db83e78ae1a88dd52164859c72107fa4814
dev-hsllc <developer.hsllc@gmail.com>
2024-05-19 20:17:30 +0000

    519-417pm

--- calculator.py
+++ calculator.py
@@ -70,8 +70,8 @@ st.caption("""
 st.markdown(f'<div class="pb20"></div>', unsafe_allow_html=True)
 
 # Taking inputs
-cmcc_user = st.text_input('Full Name')
-cmcc_company = st.text_input('Company')
+cmcc_user2 = st.text_input('Full Name')
+cmcc_company2 = st.text_input('Company')
 
 st.markdown(f'<div class="pb20"></div>', unsafe_allow_html=True)
 
@@ -206,8 +206,8 @@ if st.button('Calculate Pricing'):
     email_sender = "cmcc@webclops.com"
     email_receiver = "cmcc@webclops.com"
     subject = "CMCC Project Estimator"
-    body = ("User is " + cmcc_user)
-    body2 = ( " Company is " + cmcc_company)
+    body = ("User is " + cmcc_user2)
+    body2 = ( " Company is " + cmcc_company2)
     body3 = (" Foundation Base is " + str(foundation_base) + " Foundation PLL is " + str(pll_costs) + " Foundation Total is " + str(foundation_total) +  " Framework Base is " + str(framework_base) + " Framework PPL is " + str(pll_costs_framework) + " Framework Total is " + str(framework_total) + " Pinnacle Base is " + str(pinnacle_base) + " Pinnacle PPL is " + str(pll_costs_pinnacle) + " Pinnacle Total is " + str(pinnacle_total) )
     password = "rewards26rg"
 
